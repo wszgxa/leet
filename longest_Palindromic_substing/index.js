6433ca09949e19bd1e35f56788b581106aad994c
hiluluke <hiluluke@hotmail.com>
2016-11-01 11:05:17 +0800

    回文子字符串

--- longest_Palindromic_substing/index.js
+++ longest_Palindromic_substing/index.js
@@ -1,15 +1,28 @@
 var testString = require('./test_case').testString
 
 var longestPalindrome = function (s) {
-  var resString = s.slice(0, 1)
+  var a = s.split('')
+  var resString = a.slice(0, 1)
   var judgePalindrome = function (ss) {
-    let tagArray = []
-    for (var i = 0; i <= ss.length;i++) {
-      tagArray.push(ss[i])
+    var tag = ss
+    while (tag.length >= 2 ) {
+      if (tag.pop() !== tag.shift()) return false
+    }
+    return true
+  }
+  for (var i = 0; i < a.length; i++) {
+    if (resString.length >= a.slice(i).length) break
+    for (var j = i+1; j <= a.length; j++) {
+      var sub = a.slice(i,j)
+      if (sub.length <= resString.length) continue
+      if (judgePalindrome(sub)) {
+        resString = a.slice(i,j)
+      }
     }
   }
-  return resString
+  return resString.join('')
 }
+
 
 for (var i = 0; i < testString.length; i++) {
   console.log(longestPalindrome(testString[i]))
